abbc230f539ad38c353258e32eb27bffef0baa6f
Hugo Bullon <hugobullont@gmail.com>
2017-09-16 01:20:56 -0500

    Agregada Interfaz Grafica

--- OchoReinasGenetico/MainWindow.xaml.cs
+++ OchoReinasGenetico/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using OchoReinasGenetico.Clases;
+using System.Threading;
 
 namespace OchoReinasGenetico
 {
@@ -20,9 +22,73 @@ namespace OchoReinasGenetico
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Image queenImage;
+        private int population;
+        private GeneticAlgo geneticAlgo;
+        private int generation;
+
+        private string generationString = "Generaciones : \n";
+
         public MainWindow()
         {
             InitializeComponent();
+            queenImage = new Image();
+            queenImage.Source = new BitmapImage(new Uri(@"D:\queen.png"));
+            generation = 1;
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.population = int.Parse(PopulationTextBox.Text);
+
+            geneticAlgo = new GeneticAlgo(this.population);
+
+            this.ejecutarAlgoritmo();
+        }
+
+        private void imprimirReina(int fila, int columna)
+        {
+            Button button = (Button)ChessGrid.Children
+            .Cast<UIElement>()
+            .First(e => Grid.GetRow(e) == fila && Grid.GetColumn(e) == columna);
+            button.Content = queenImage;
+        }
+
+        private void agregarCromosoma(Chromosome chromo)
+        {
+            this.generationString = this.generationString + generation +": " + chromo.displayChromosome() + "\n";
+            this.imprimirConsola();
+        }
+
+        private void imprimirConsola()
+        {
+            textConsole.Text = this.generationString;
+        }
+
+        private void ejecutarAlgoritmo()
+        {
+            while(geneticAlgo.getFittestChromosome().getFitness() < 0)
+            {
+                geneticAlgo.naturalSelection();
+                this.buscarPosicionesReinas(geneticAlgo.getFittestChromosome());
+                this.agregarCromosoma(geneticAlgo.getFittestChromosome());
+                generation++;
+            }
+            this.done();
+        }
+
+        private void done()
+        {
+            this.generationString = this.generationString + "TERMINADO";
+            this.imprimirConsola();
+        }
+
+        private void buscarPosicionesReinas(Chromosome chromo)
+        {
+            for(int i=0;i<8;i++)
+            {
+                this.imprimirReina(chromo.getGenes(i), i);
+            }
         }
     }
 }
